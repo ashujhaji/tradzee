92f16197a7f6818a67675ee1c299e23dc6ad8af4
Ashutosh Jha <ashujha.1996@gmail.com>
2019-12-01 21:04:16 +0530

    rudandant files removed

--- app/src/main/java/com/pixerapps/tradzee/ui/activity/home/fragments/orders/OrdersPresenter.kt
+++ app/src/main/java/com/pixerapps/tradzee/ui/activity/home/fragments/orders/OrdersPresenter.kt
@@ -3,6 +3,7 @@ package com.pixerapps.tradzee.ui.activity.home.fragments.orders
 import android.content.Context
 import com.pixerapps.tradzee.data.local.TradzeeDb
 import com.pixerapps.tradzee.mvp.BaseMvpPresenterImpl
+import rx.schedulers.Schedulers
 
 class OrdersPresenter : BaseMvpPresenterImpl<OrdersContract.View>(),
     OrdersContract.Presenter {
